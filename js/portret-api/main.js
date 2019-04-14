1c627202da736339664a0946b38566e8078727a9
Jens Wienstroer <jens.wienstroer@meelogic.com>
2019-04-14 12:56:17 +0100

    fixed picture ordering

--- js/portret-api/main.js
+++ js/portret-api/main.js
@@ -30,7 +30,7 @@ var server = app.listen(8081, function () {
    var host = server.address().address;
    var port = server.address().port;
    host = (host == "::") ? "127.0.0.1" : host;
-   console.log("Portret REST API listening at http://%s:%s", host, port)
+   console.log("Portret REST API listening at http://%s:%s", host, port);
 });
 
 /**
@@ -59,7 +59,8 @@ portret.getCollections = function () {
  * @return {Array}
  */
 portret.getPics = function (collection) {
-    var result = {};
+    var result = {},
+        numberedFileNames = true;
     result.name = collection;
     result.pics = [];
     function walkDir(curPath) {
@@ -67,6 +68,7 @@ portret.getPics = function (collection) {
         for (var f in files) {
             var curFile = path.join(curPath, files[f]);
             if (fs.statSync(curFile).isFile() && portret.fileTypes.indexOf(path.extname(curFile).toLowerCase()) != -1) {
+                numberedFileNames &= portret.isNumberedFileName(files[f]);
                 result.pics.push(files[f]);
             } else if (fs.statSync(curFile).isDirectory()) {
                 walkDir(curFile);
@@ -75,5 +77,20 @@ portret.getPics = function (collection) {
     }
     walkDir(path.join(portret.collectionPath, collection));
     result.count = result.pics.length;
+    if (numberedFileNames) {
+        console.log("Files in collection '%s' are numbered and will be played back in that order.", collection);
+        result.pics.sort(portret.compareNumberedFileNames);
+    }
     return result;
+};
+
+portret.isNumberedFileName = function (file) {
+    var namepart = path.basename(file, path.extname(file));
+    return (namepart.match(/^\d+$/) != null);
+};
+
+portret.compareNumberedFileNames = function (a, b) {
+    var file_num_a = path.basename(a, path.extname(a));
+    var file_num_b = path.basename(b, path.extname(b));
+    return file_num_a - file_num_b;
 };
